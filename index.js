f88bb1fd36240befd1c54289dceaf0d63fbc1dae
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-01-01 12:40:36 -0300

    Initial template

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config();
+const connection = require('./database/database')
+
+// View engine
+app.set('view engine', 'ejs')
+
+// Static
+app.use(express.static('public'))
+
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Databse
+connection.authenticate().then(() => {
+  console.log('Conexão iniciada')
+}).catch((error) => {
+  console.log(error)
+})
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, (() => {
+  console.log('App rodando!')
+}))
