b7d9b623ae8b4b633b59edd86ef57b00bffe1bf0
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-01-04 13:30:16 -0300

    feat: config database 1-n-1 1-n-m

--- articles/Articles.js
+++ articles/Articles.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const connection = require('../database/database')
+const Category = require('../categories/Category')
 
 const Article = connection.define('articles', {
   title: {
@@ -15,5 +16,8 @@ const Article = connection.define('articles', {
     allowNull: false
   }
 })
+
+Category.hasMany(Article) // Uma categoria tem muitos artigos 1-P-M
+Article.belongsTo(Category) // Um artigo pertence a uma categoria 1-P-1
 
 module.exports = Article;
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const connection = require('./database/database')
 const categoriesController = require('./categories/CategoriesController')
 const articlesController = require('./articles/ArticlesController')
+const Article = require('./articles/Articles')
+const Category = require('./categories/Category')
+
 
 // View engine
 app.set('view engine', 'ejs')
