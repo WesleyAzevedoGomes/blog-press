cef9bae480f8a7a65cc05556f7fe74f282468fca
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-01-11 14:40:47 -0300

    feat: save articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const router = express.Router();
+const Category = require('../categories/Category')
+const Article = require('./Articles')
+const slugify = require('slugify')
+
 
 router.get('/articles', (req, res) => {
   res.send("Rota de artigos")
 })
 
 router.get('/admin/articles/new', (req, res) => {
-  res.render('admin/articles/new')
+  Category.findAll().then((categories) => {
+    res.render('admin/articles/new', {categories})
+  })  
 })
 
+router.post('/articles/save', (req, res) => {
+  const title = req.body.title;
+  const bodyText = req.body.body;
+  const category = req.body.category;
+
+  Article.create({
+    title,
+    slug: slugify(title),
+    body: bodyText,
+    categoryId: category
+
+  })
+})
 
 module.exports = router;
