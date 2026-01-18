d81fb255a38151413913830b4e90a97301656f01
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-01-18 19:39:10 -0300

    feat: form register users

--- user/UsersController.js
+++ user/UsersController.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const User = require('./User')
 
 router.get('/admin/users', (req, res) => {
-  res.render('listagem de usuarios')
+  res.send('listagem de usuarios')
 })
 
 router.get('/admin/users/create', (req, res) => {
   res.render('admin/users/create')
 })
 
+router.post('/users/create', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
+  res.json({email, password})
+})
 
 module.exports = router;
