81f055314d7e4ee98c7650dbdc194f0e97ae5d36
Wesley Azevedo <wesleyazevedogomes.contato@gmail.com>
2026-01-18 20:49:57 -0300

    feat: password encryption

--- user/UsersController.js
+++ user/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const User = require('./User')
+const bcrypt = require('bcryptjs')
 
 router.get('/admin/users', (req, res) => {
   res.send('listagem de usuarios')
@@ -14,7 +15,16 @@ router.post('/users/create', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  res.json({email, password})
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt)
+  User.create({
+    email,
+    password: hash
+  }).then(() => {
+    res.redirect('/')
+  }).catch((err) => {
+    res.redirect('/')
+  })
 })
 
 module.exports = router;
